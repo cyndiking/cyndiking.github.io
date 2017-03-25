db559f9e0b3fa5051678472ad2fafd472109b1b8
cyndiking <cyndiking@163.com>
2017-03-25 21:19:11 +0800

    自己电脑操作

---
+++ js/index.js
@@ -0,0 +1,35 @@
+$(function(){
+	// 头部导航
+	$(window).scroll(function() {
+        if($(document).scrollTop() > 50){
+        	$("#header").addClass('header-action')
+        }else{
+        	$("#header").removeClass('header-action')
+        }
+
+        if($(document).scrollTop() > 200){
+            $("#back-top").css({"display":"block"})
+            var dis = $(document).height()-$(window).height()-$(document).scrollTop();
+            if(dis< $("#footer").height()-$("#back-top").outerHeight()){
+                $("#back-top").addClass('top-other');
+            }else{
+                $("#back-top").removeClass('top-other');
+            }
+        }else{
+           $("#back-top").css({"display":"none"})
+        }
+    });
+    
+    $("#back-top").click(function(){
+        $("html,body").animate({scrollTop:0},800)
+    })
+
+    $(".contact-me").click(function(){
+        $(".my-git").addClass('contact-git');
+        $(".my-email").addClass('contact-email');
+        setTimeout(function(){
+            $(".my-git").removeClass('contact-git');
+            $(".my-email").removeClass('contact-email');
+        },9000)
+    })
+})
